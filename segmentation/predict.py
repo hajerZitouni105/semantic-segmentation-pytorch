35b10dd396e86aad748c62c207530fcb9cd650b7
Richard Kuo <rkuo2000@gmail.com>
2020-12-28 06:40:04 +0800

    Update predict.py

--- segmentation/predict.py
+++ segmentation/predict.py
@@ -102,7 +102,9 @@ def predict(model, input_path, output_path, colors=class_colors):
 	lbl_pred = lbl_pred.transpose((1, 2, 0))
 	n_classes = np.max(lbl_pred)
 	lbl_pred = lbl_pred.reshape(model_height, model_width)
-
+        print(type(lbl_pred)
+        print(lbl_pred.shape)
+        print(lbl_pred)
 	seg_img = convert_seg_gray_to_color(lbl_pred, n_classes, colors=colors)
 
 	if model_width != ori_width or model_height != ori_height:
